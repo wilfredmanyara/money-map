babf85cce47006f98d448e96b32585ad24f0c6d6
wilfredmanyara <manyarawilly46@gmail.com>
2024-06-14 12:51:58 +0300

    feat(money-map): manage section completed

--- app/(dashboard)/_components/CreateCategoryDialog.tsx
+++ app/(dashboard)/_components/CreateCategoryDialog.tsx
@@ -13,7 +13,7 @@ import {
 } from "@/schema/categories";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { CircleOff, Loader2, PlusSquare } from "lucide-react";
-import React, { useCallback, useState } from "react";
+import React, { ReactNode, useCallback, useState } from "react";
 import { useForm } from "react-hook-form";
 import Picker from "@emoji-mart/react";
 import data from "@emoji-mart/data";
@@ -26,9 +26,10 @@ import { useTheme } from "next-themes";
 interface Props {
   type: TransactionType;
   successCallback: (category: Category) => void;
+  trigger?: ReactNode
 }
 
-function CreateCategoryDialog({ type, successCallback }: Props) {
+function CreateCategoryDialog({ type, successCallback, trigger }: Props) {
   const [open, setOpen] = useState(false);
   const form = useForm<CreateCategorySchemaType>({
     resolver: zodResolver(CreateCategorySchema),
@@ -81,13 +82,13 @@ function CreateCategoryDialog({ type, successCallback }: Props) {
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
-        <Button
+        {trigger ? trigger : <Button
           variant={"ghost"}
           className="flex border-separate items-center justify-start rounded-none border-b px-3 py-3 text-muted-foreground"
         >
           <PlusSquare className="mr-2 h-4 w-4" />
           Create new
-        </Button>
+        </Button>}
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
