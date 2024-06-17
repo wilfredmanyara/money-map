910c6452b3096b6337a58ea7ed23bc3bfde7b4e7
wilfredmanyara <manyarawilly46@gmail.com>
2024-06-17 17:31:04 +0300

    reverting the changes

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,15 +1,12 @@
 import Navbar from "@/components/Navbar";
-import { ClerkProvider } from "@clerk/nextjs";
 import React, { ReactNode } from "react";
 
 function layout({ children }: { children: ReactNode }) {
   return (
-    <ClerkProvider>
     <div className="relative flex h-screen w-full flex-col">
         <Navbar />
       <div className="w-full">{children}</div>
     </div>
-    </ClerkProvider>
   );
 }
 
